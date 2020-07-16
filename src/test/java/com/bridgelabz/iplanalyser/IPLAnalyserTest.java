f8c0848f75444225106681d8e1e17ab9ebd00c49
“Shubham” <shubhampattar2@gmail.com>
2020-07-16 22:49:21 +0530

    REFACTORED Test Case To Get Batsman Who Hits Most Sixes And Fours in IPL 2019.

--- src/test/java/com/bridgelabz/iplanalyser/IPLAnalyserTest.java
+++ src/test/java/com/bridgelabz/iplanalyser/IPLAnalyserTest.java
@@ -60,18 +60,12 @@ public class IPLAnalyserTest {
 
     // UC3
     @Test
-    public void givenIPLMostRunsCSVFile_ShouldReturn_PlayersNameWhoHitsMaximum_SixesAndFours() {
-        try {
-            IPLAnalyser iplAnalyser = new IPLAnalyser(IPLAnalyser.PlayerType.BATSMAN);
-            iplAnalyser.loadIPLData(IPLAnalyser.PlayerType.BATSMAN, MOST_RUNS_CSV_FILE_PATH);
-            IPLMostRunsCSV maximumFourHitter = iplAnalyser.getMaximumFourHitter();
-            IPLMostRunsCSV maximumSixHitter = iplAnalyser.getMaximumSixHitter();
-            Assert.assertThat(maximumFourHitter.player, CoreMatchers.is("Shikhar Dhawan"));
-            Assert.assertThat(maximumSixHitter.player, CoreMatchers.is("Andre Russell"));
-        } catch (IPLAnalyserException e) {
-            System.out.println("Fail");
-            e.printStackTrace();
-        }
+    public void givenIPLMostRunsCSVFile_ShouldReturn_PlayersNameWhoHitsMaximum_SixesAndFours() throws IPLAnalyserException {
+        IPLAnalyser iplAnalyser = new IPLAnalyser();
+        iplAnalyser.loadIPLData(IPLAnalyser.PlayerType.BATSMAN, MOST_RUNS_CSV_FILE_PATH);
+        String player = iplAnalyser.getSortedData(SortField.SIXES_AND_FOURS);
+        IPLAnalyserDAO[] iplCSV = new Gson().fromJson(player, IPLAnalyserDAO[].class);
+        Assert.assertEquals("Andre Russell", iplCSV[0].playerName);
     }
 
     // UC4
